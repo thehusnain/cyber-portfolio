e4151c209a18f0a5b2a1864cf9e3e71493051de0
Husnain <huxnain.cs@gmail.com>
2026-06-12 11:41:02 +0500

    changed portfolio design

--- src/components/ParticlesContainer.jsx
+++ src/components/ParticlesContainer.jsx
@@ -1,21 +1,26 @@
-import React, { useCallback } from "react";
-import { Particles } from "react-tsparticles";
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadFull } from "tsparticles";
 
 const ParticlesContainer = () => {
-  // init
-  const particlesInit = useCallback(async (engine) => {
-    await loadFull(engine);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  const particlesLoaded = useCallback(async () => {}, []);
+  if (!init) {
+    return null;
+  }
 
   return (
     <Particles
       className="w-full h-full absolute translate-z-0"
       id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
       options={{
         fullScreen: { enable: false },
         background: {
